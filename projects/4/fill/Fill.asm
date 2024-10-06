468def600bde73093e9c829dc43d253311c71a1e
Chaithanya J <krishnachaithanya826@gmail.com>
2024-10-06 17:11:42 +0530

    Fill.asm

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -3,10 +3,43 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
+(LOOP)
+@SCREEN 
+D=A
+@screen_ptr 
+M=D
+@KBD 
+D=M 
+@WHITE 
+D;JEQ
+@BLACK
+0;JMP
 
+(BLACK) 
+@color 
+M=-1 
+@PAINT
+0;JMP
 
+(WHITE)
+@color 
+M=0 
+@PAINT
+0;JMP
 
-
-
-
-
+(PAINT)
+@screen_ptr 
+D=M 
+@24575 
+D=A-D 
+@LOOP 
+D;JLT
+@color 
+D=M 
+@screen_ptr
+A=M
+M=D 
+@screen_ptr 
+M=M+1 
+@PAINT 
+0;JMP  
