b37e597fce8f6fdefdfe0c71d421789c3a97f8d0
Chaithanya J <krishnachaithanya826@gmail.com>
2025-03-01 15:31:57 +0530

    translated basic vm programs in project8 to asm

--- projects/8/ProgramFlow/BasicLoop/BasicLoop.asm
+++ projects/8/ProgramFlow/BasicLoop/BasicLoop.asm
@@ -1,11 +1,9 @@
-// push constant 0
 @0
 D=A
 @SP
 M=M+1
 A=M-1
 M=D
-// pop local 0         // sum = 0
 @0
 D=A
 @LCL
@@ -19,9 +17,7 @@ D=M
 @addr
 A=M
 M=D
-// label LOOP
 (LOOP)
-// push argument 0
 @0
 D=A
 @ARG
@@ -32,7 +28,6 @@ A=M
 M=D
 @SP
 M=M+1
-// push local 0
 @0
 D=A
 @LCL
@@ -43,15 +38,13 @@ A=M
 M=D
 @SP
 M=M+1
-// add
 @SP
 M=M-1
 A=M
 D=M
 A=A-1
 M=M+D
-// pop local 0	        // sum = sum + n
-@0	
+@0
 D=A
 @LCL
 D=D+M
@@ -64,7 +57,6 @@ D=M
 @addr
 A=M
 M=D
-// push argument 0
 @0
 D=A
 @ARG
@@ -75,21 +67,18 @@ A=M
 M=D
 @SP
 M=M+1
-// push constant 1
 @1
 D=A
 @SP
 M=M+1
 A=M-1
 M=D
-// sub
 @SP
 M=M-1
 A=M
 D=M
 A=A-1
 M=M-D
-// pop argument 0      // n--
 @0
 D=A
 @ARG
@@ -103,7 +92,6 @@ D=M
 @addr
 A=M
 M=D
-// push argument 0
 @0
 D=A
 @ARG
@@ -114,14 +102,12 @@ A=M
 M=D
 @SP
 M=M+1
-// if-goto LOOP        // if n > 0, goto LOOP
 @SP
 M=M-1
 A=M
 D=M
 @LOOP
 D;JNE
-// push local 0        // else, pushes sum to the stack's top
 @0
 D=A
 @LCL
--- projects/8/ProgramFlow/FibonacciSeries/FibonacciSeries.asm
+++ projects/8/ProgramFlow/FibonacciSeries/FibonacciSeries.asm
@@ -1,4 +1,3 @@
-// push argument 1         // sets THAT, the base address of the
 @1
 D=A
 @ARG
@@ -9,21 +8,18 @@ A=M
 M=D
 @SP
 M=M+1
-// pop pointer 1           // that segment, to argument[1]
 @SP
 M=M-1
 A=M
 D=M
 @THAT
 M=D
-// push constant 0         // sets the series' first and second
 @0
 D=A
 @SP
 M=M+1
 A=M-1
 M=D
-// pop that 0              // elements to 0 and 1, respectively
 @0
 D=A
 @THAT
@@ -37,14 +33,12 @@ D=M
 @addr
 A=M
 M=D
-// push constant 1
 @1
 D=A
 @SP
 M=M+1
 A=M-1
 M=D
-// pop that 1
 @1
 D=A
 @THAT
@@ -58,7 +52,6 @@ D=M
 @addr
 A=M
 M=D
-// push argument 0         // sets n, the number of remaining elements
 @0
 D=A
 @ARG
@@ -69,21 +62,18 @@ A=M
 M=D
 @SP
 M=M+1
-// push constant 2         // to be computed, to argument[0] minus 2,
 @2
 D=A
 @SP
 M=M+1
 A=M-1
 M=D
-// sub                     // since 2 elements were already computed.
 @SP
 M=M-1
 A=M
 D=M
 A=A-1
 M=M-D
-// pop argument 0
 @0
 D=A
 @ARG
@@ -97,9 +87,7 @@ D=M
 @addr
 A=M
 M=D
-// label LOOP
 (LOOP)
-// push argument 0
 @0
 D=A
 @ARG
@@ -110,19 +98,15 @@ A=M
 M=D
 @SP
 M=M+1
-// if-goto COMPUTE_ELEMENT // if n > 0, goto COMPUTE_ELEMENT
 @SP
 M=M-1
 A=M
 D=M
 @COMPUTE_ELEMENT
 D;JNE
-// goto END                // otherwise, goto END
 @END
 0;JMP
-// label COMPUTE_ELEMENT
 (COMPUTE_ELEMENT)
-// push that 0
 @0
 D=A
 @THAT
@@ -133,7 +117,6 @@ A=M
 M=D
 @SP
 M=M+1
-// push that 1
 @1
 D=A
 @THAT
@@ -144,14 +127,12 @@ A=M
 M=D
 @SP
 M=M+1
-// add
 @SP
 M=M-1
 A=M
 D=M
 A=A-1
 M=M+D
-// pop that 2
 @2
 D=A
 @THAT
@@ -165,7 +146,6 @@ D=M
 @addr
 A=M
 M=D
-// push pointer 1
 @4
 D=M
 @SP
@@ -173,28 +153,24 @@ A=M
 M=D
 @SP
 M=M+1
-// push constant 1
 @1
 D=A
 @SP
 M=M+1
 A=M-1
 M=D
-// add
 @SP
 M=M-1
 A=M
 D=M
 A=A-1
 M=M+D
-// pop pointer 1
 @SP
 M=M-1
 A=M
 D=M
 @THAT
 M=D
-// push argument 0
 @0
 D=A
 @ARG
@@ -205,21 +181,18 @@ A=M
 M=D
 @SP
 M=M+1
-// push constant 1
 @1
 D=A
 @SP
 M=M+1
 A=M-1
 M=D
-// sub
 @SP
 M=M-1
 A=M
 D=M
 A=A-1
 M=M-D
-// pop argument 0
 @0
 D=A
 @ARG
@@ -233,8 +206,6 @@ D=M
 @addr
 A=M
 M=D
-// goto LOOP
 @LOOP
 0;JMP
-// label END
 (END)
